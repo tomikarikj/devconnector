a2943955b33e13bf94c298d17df87293e526e668
Tomi Karikj <karikjtomi@gmail.com>
2019-06-06 23:31:44 +0200

    Added logic for removing the user's posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -153,6 +154,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Public
 router.delete('/', auth, async (req, res) => {
   try {
+    // Remove user's posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
